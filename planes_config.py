1e4e8d334d1f01079ee1ffd8600c541e46374fc8
Will-PC <william@whcls.co.uk>
2021-10-02 13:04:52 +0100

    Config changes for mode source selection

--- planes_config.py
+++ planes_config.py
@@ -1,8 +1,9 @@
 # Latitude and longitude where the device is installed.
-# AIRCRAFT_JSON set to dump1090 aircraft.json location
-
+# DUMP1090 set to dump1090 aircraft.json location
+# FR24 set to Flightradar24 feeder flights.json location
 
 MY_LAT = 0
 MY_LONG = 0
 DUMP1090 = "http://0.0.0.0/dump1090/data/aircraft.json"
 FR24 = "http://0.0.0.0:8754/flights.json"
+SOURCE = "DUMP1090" # DUMP1090 or FR24
